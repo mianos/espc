96e8488b65cf2297bb1ba5cfa0ad040ac84fb0cb
Rob Fowler <rfo@mianos.com>
2024-03-23 19:28:47 +1100

    cleanup

--- main/counter.c
+++ main/counter.c
@@ -3,7 +3,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/pulse_cnt.h"  // Use the appropriate PCNT driver header
-// #include "driver/pcnt.h"
 #include "driver/ledc.h"
 #include "esp_log.h"
 #include "esp_err.h"
@@ -11,6 +10,7 @@
 
 
 static const char *TAG = "counter";
+
 #define PCNT_INPUT_SIG_IO   20 // Pulse Input GPIO
 #define PCNT_INPUT_SIG_TRIGGER   GPIO_NUM_22 // Pulse Input GPIO
 
@@ -28,7 +28,6 @@ static bool example_pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_even
 	cnt++;
     BaseType_t high_task_wakeup;
     QueueHandle_t queue = (QueueHandle_t)user_ctx;
-    // send event data to queue, from this interrupt callback
     xQueueSendFromISR(queue, &(edata->watch_point_value), &high_task_wakeup);
     return (high_task_wakeup == pdTRUE);
 }
@@ -36,31 +35,23 @@ static bool example_pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_even
 static void IRAM_ATTR ctrl_signal_isr_handler(void* arg)
 {
 	trigger_cnt++;
-    // Disable the counter
     pcnt_unit_stop(pcnt_unit);
 }
 
 void pcnt(void)
 {
-    ESP_LOGI(TAG, "install pcnt unit");
     pcnt_unit_config_t unit_config = {
         .high_limit = EXAMPLE_PCNT_HIGH_LIMIT,
         .low_limit = EXAMPLE_PCNT_LOW_LIMIT,
     };
     ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
-
-    ESP_LOGI(TAG, "install pcnt channel");
     pcnt_chan_config_t chan_a_config = {
         .edge_gpio_num = PCNT_INPUT_SIG_IO,
         .level_gpio_num = PCNT_INPUT_SIG_TRIGGER
     };
     pcnt_channel_handle_t pcnt_chan_a = NULL;
     ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_a_config, &pcnt_chan_a));
-
-    ESP_LOGI(TAG, "set edge action for pcnt channel");
     ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_a, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_HOLD));
-
-    ESP_LOGI(TAG, "add watch point and register callback");
     ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, EXAMPLE_PCNT_HIGH_LIMIT));
     pcnt_event_callbacks_t cbs = {
         .on_reach = example_pcnt_on_reach,
@@ -84,11 +75,8 @@ void pcnt(void)
     // Hook the ISR handler for the control signal GPIO
     ESP_ERROR_CHECK(gpio_isr_handler_add(PCNT_INPUT_SIG_TRIGGER, ctrl_signal_isr_handler, NULL));
 
-    ESP_LOGI(TAG, "enable pcnt unit");
     ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
-    ESP_LOGI(TAG, "clear pcnt unit");
     ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
-    ESP_LOGI(TAG, "start pcnt unit");
     ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
 }
 
@@ -97,17 +85,15 @@ void app_main() {
     queue = xQueueCreate(10, sizeof(int));
 
 	extern void init_ledc_square_wave();
+	extern void start_pulse_timer();
 
     init_ledc_square_wave();
-    pcnt();
-
-	extern void start_pulse_timer();
     start_pulse_timer();
+    pcnt();
 
     // Report counter value
     int pulse_count = 0;
     int event_count = 0;
-    int pin22_state = 0;
 
     while (1) {
         if (xQueueReceive(queue, &event_count, pdMS_TO_TICKS(1000))) {
@@ -116,13 +102,6 @@ void app_main() {
             ESP_ERROR_CHECK(pcnt_unit_get_count(pcnt_unit, &pulse_count));
             ESP_LOGI(TAG, "Pulse count: %d", pulse_count);
         }
-
-        // Read the state of pin 22
-        pin22_state = gpio_get_level(GPIO_NUM_22);
-
-        // Print the state of pin 22 on the console
-        ESP_LOGI(TAG, "Pin 22 state: %d", pin22_state);
-
         vTaskDelay(pdMS_TO_TICKS(500)); 
 
         printf("cnt is %d trigger_cnt %d\n", cnt, trigger_cnt);
